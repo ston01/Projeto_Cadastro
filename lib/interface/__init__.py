f609e798b573fb23d8b8d2771f5b73886014fd9d
Emanuel de Carvalho Terra <emanuelt783@gmail.com>
2026-05-03 15:07:21 -0300

    Função de opção e organização da lógica do menu
    - Criada função de interface para exibir o título das opções.
    - Encaixando a lógica do cadastro da (opção 2) pra dentro da condicional correspondente.
    - Adicionado aviso visual para encerramento do programa (opção 3).

--- lib/interface/__init__.py
+++ lib/interface/__init__.py
@@ -1,8 +1,19 @@
+menu_principal = 'MENU PRINCIPAL'
+largura = 40
+
+
+
 def MenuPrincipal():
-    print('='*40)
-    print('             MENU PRINCIPAL             ')
+    print('='*largura)
+    print(menu_principal.center(largura))
     print('='*40)
     print(' [ 1 ] - Ver pessoas cadastradas')
     print(' [ 2 ] - Cadastrar nova pessoa')
     print(' [ 3 ] - Sair do sistema')
-    print('-'*40)
+    print('='*40)
+
+
+def Opção(msg, larg=40):
+    print('-'*larg)
+    print(f'{msg.center(larg)}')
+    print('-'*larg)
--- main.py
+++ main.py
@@ -1,8 +1,18 @@
-from lib.interface import MenuPrincipal
+from lib.interface import MenuPrincipal, Opção
 geral = []
 cadastros = {}
 while True:
     MenuPrincipal()
-    cadastros['Nome'] = str(input('Nome: '))
-    cadastros['Idade'] = int(input('Idade: '))
-    geral.append(cadastros.copy())
+    escolha = int(input('Opção escolhida: '))
+    if escolha == 1:
+        for pessoa in geral:
+            Opção('OPÇÃO 1')
+            print(f'{pessoa['Nome']} \t {pessoa['Idade']} anos')
+    if escolha == 2:
+        Opção('OPÇÃO 2')
+        cadastros['Nome'] = str(input('Nome: '))
+        cadastros['Idade'] = int(input('Idade: '))
+        geral.append(cadastros.copy())
+    if escolha == 3:
+        Opção('Saindo do programa... Até breve!')
+        break
