b11db5cbc724eda4581364e5f9a55a0ceb56530a
Emanuel de Carvalho Terra <emanuelt783@gmail.com>
2026-05-03 13:55:33 -0300

    Criação da função de interface
    Criei aqui a função menu principal para economizar linhas no meu programa principal

---
+++ lib/interface/__init__.py
@@ -0,0 +1,8 @@
+def MenuPrincipal():
+    print('='*40)
+    print('             MENU PRINCIPAL             ')
+    print('='*40)
+    print(' [ 1 ] - Ver pessoas cadastradas')
+    print(' [ 2 ] - Cadastrar nova pessoa')
+    print(' [ 3 ] - Sair do sistema')
+    print('-'*40)
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+from lib.interface import MenuPrincipal
 geral = []
 cadastros = {}
 while True:
+    MenuPrincipal()
     cadastros['Nome'] = str(input('Nome: '))
     cadastros['Idade'] = int(input('Idade: '))
     geral.append(cadastros.copy())
