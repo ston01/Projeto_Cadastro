22c747e727523af185affa9259122e5d64ba803a
Emanuel de Carvalho Terra <emanuelt783@gmail.com>
2026-05-12 13:47:52 -0300

    refactor: organiza ordem do menu e muda nomes
    Alterada a ordem das opções para seguir o seguinte fluxo (Listar -> Pesquisar -> Novo -> Atualizar -> Deletar).

--- main.py
+++ main.py
@@ -8,19 +8,12 @@ while True:
     menu_principal()
     escolha = validar_int('Opção escolhida: ')
     if escolha == 1:
-        cabeçalho('OPÇÃO 1')
+        cabeçalho('CADASTROS')
         pessoas = listar_usuarios()
         for pessoa in pessoas:
             print(f"{pessoa['id']:<3} | {pessoa['nome']:<15} | {pessoa['idade']:<3} anos | {pessoa['email']:<30} | {pessoa['celular']}")
     elif escolha == 2:
-        cabeçalho('OPÇÃO 2')
-        cadastros['nome'] = validar_str('Nome: ')
-        cadastros['idade'] = validar_int('Idade: ')
-        cadastros['email'] = validar_email('Email: ')
-        cadastros['celular'] = validar_celular('Celular: ')
-        inserir_usuario(cadastros['nome'], cadastros['idade'], cadastros['email'], cadastros['celular'])
-    elif escolha == 3:
-        cabeçalho('OPÇÃO 3')
+        cabeçalho('PESQUISAR CADASTRO')
         nome_busca = validar_str('Digite o nome ou parte dele: ')
         resultados = pesquisar_usuario(nome_busca)
         if resultados:
@@ -28,17 +21,15 @@ while True:
                 print(f"{pessoa['id']:<3} | {pessoa['nome']:<15} | {pessoa['idade']:<3} anos | {pessoa['email']:<30} | {pessoa['celular']}")
         else:
             print(f'Não encontramos ninguém com o nome "{nome_busca}"')
+    elif escolha == 3:
+        cabeçalho('NOVO CADASTRO')
+        cadastros['nome'] = validar_str('Nome: ')
+        cadastros['idade'] = validar_int('Idade: ')
+        cadastros['email'] = validar_email('Email: ')
+        cadastros['celular'] = validar_celular('Celular: ')
+        inserir_usuario(cadastros['nome'], cadastros['idade'], cadastros['email'], cadastros['celular'])
     elif escolha == 4:
-        cabeçalho('OPÇÃO 4')
-        id_excluir = validar_int('Digite o ID do usuário que deseja remover: ')
-        certeza = str(input(f'Tem certeza que deseja apagar o ID {id_excluir}? [S/N]: ')).upper().strip()
-        if certeza == 'S':
-            deletar_usuario(id_excluir)
-            print(f'Usuário {id_excluir} removido com sucesso!')
-        else:
-            print('Operação cancelada.') 
-    elif escolha == 5:
-        cabeçalho('OPÇÃO 5')
+        cabeçalho('ATUALIZAR CADASTRO')
         id_edit = validar_int('Digite o ID: ')
         usuario = buscar_usuario(id_edit)
         if usuario:
@@ -50,6 +41,15 @@ while True:
             atualizar_usuario(id_edit, novo_nome, nova_idade, novo_email, novo_celular)
         else:
             print('ID não encontrado.')
+    elif escolha == 5:
+        cabeçalho('DELETAR CADASTRO')
+        id_excluir = validar_int('Digite o ID do usuário que deseja remover: ')
+        certeza = str(input(f'Tem certeza que deseja apagar o ID {id_excluir}? [S/N]: ')).upper().strip()
+        if certeza == 'S':
+            deletar_usuario(id_excluir)
+            print(f'Usuário {id_excluir} removido com sucesso!')
+        else:
+            print('Operação cancelada.') 
     elif escolha == 6:
         cabeçalho('Saindo do programa... Até breve!')
         break
